8fa973c35b1dfa3598d04b10c883f2c4d38dda71
axell-brendow <breno.axel@gmail.com>
2020-04-24 03:36:27 -0300

    Create portfolio section

---
+++ src/containers/Home/Portfolio/data.ts
@@ -0,0 +1,55 @@
+import aluraHTML5 from "assets/portfolio-alura-html5-css3.png";
+import graphs from "assets/portfolio-graphs.png";
+import omnistack10DevRadarFrontend from "assets/portfolio-omnistack10-devradar-frontend.jpeg";
+import omnistack10DevRadarMobile from "assets/portfolio-omnistack10-devradar-mobile.jpeg";
+import pingCRCMaquinas from "assets/portfolio-pingcrc-maquinas.png";
+import starWarsLDDM from "assets/portfolio-star-wars-lddm.png";
+import starWarsLevelUp from "assets/portfolio-starwarsapp-levelup.png";
+import superStore from "assets/portfolio-super-store.png";
+
+export const images = [
+  {
+    src: superStore,
+    alt: "Game store PWA",
+    link: "https://axell-brendow.github.io/super-store/",
+  },
+  {
+    src: aluraHTML5,
+    alt: "Alura course, portfolio sample using just HTML and CSS",
+    link: "https://axell-brendow.github.io/html5-css3-ii-alura/",
+  },
+  {
+    src: omnistack10DevRadarFrontend,
+    alt: "DevRadar front end",
+    link:
+      "https://www.linkedin.com/feed/update/urn:li:activity:6624636245584818176/",
+  },
+  {
+    src: omnistack10DevRadarMobile,
+    alt: "DevRadar mobile app",
+    link:
+      "https://www.linkedin.com/feed/update/urn:li:activity:6624636245584818176/",
+  },
+  {
+    src: starWarsLevelUp,
+    alt: "StarWarsApp front end LevelUp",
+    link: "https://axell-brendow.github.io/StarWarsApp/",
+  },
+  {
+    src: starWarsLDDM,
+    alt:
+      "StarWarsApp mobile app for the discipline Development Laboratory for Mobile Devices",
+    link: "https://www.youtube.com/watch?v=-ZRwlYT1O0U&feature=youtu.be",
+  },
+  {
+    src: pingCRCMaquinas,
+    alt:
+      "PingCRC an internal PUC Minas application that I helped to construct from 0 to deploy",
+    link: "",
+  },
+  {
+    src: graphs,
+    alt: "A work that I made for the discipline Graphs",
+    link: "https://axell-brendow.github.io/graphs/",
+  },
+];
---
+++ src/containers/Home/Portfolio/index.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import styled from "styled-components";
+
+import { sectionsData } from "containers/Home";
+
+import Section from "components/Section";
+import TitleAndCodeDivider from "components/TitleAndCodeDivider";
+
+import { images } from "./data";
+
+const ImageContainer = styled.a`
+  img {
+    max-height: 80vh;
+    max-width: 100%;
+  }
+`;
+
+const Image = ({ src, alt, link }: Record<string, string>) => (
+  <ImageContainer href={link} key={src} target="_blank">
+    <img src={src} alt={alt} title={alt} className="rounded" />
+  </ImageContainer>
+);
+
+const ImagesContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 40px;
+  text-align: center;
+`;
+
+const color = "white";
+
+const Portfolio: React.FC = () => {
+  return (
+    <Section
+      id={sectionsData.portfolio.link}
+      backgroundColor="purple"
+      fontSize="1.5em"
+      color={color}
+      padding="100px 20px 40px"
+      sectionLink={sectionsData.portfolio.link}
+    >
+      <TitleAndCodeDivider title="Portfolio" color={color} />
+      <ImagesContainer className="mt-5">{images.map(Image)}</ImagesContainer>
+    </Section>
+  );
+};
+
+export default Portfolio;
