879b7f5b456bbb7b770aa07ab58f8049fe13b2ae
axell-brendow <breno.axel@gmail.com>
2020-04-24 03:48:54 -0300

    Move project to deploy app

---
+++ precache-manifest.b6b41dda52dc76c63582e2d5449ecdb3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f67acb1e85d7c6db47954b97e09cd271",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2cbb5f84f612ed4f199",
+    "url": "/static/js/2.f5973092.chunk.js"
+  },
+  {
+    "revision": "b92b369c485a56d25a5a7495ecf46b59",
+    "url": "/static/js/2.f5973092.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e60316c67e12c23f729f",
+    "url": "/static/js/main.6a6501ad.chunk.js"
+  },
+  {
+    "revision": "160116e6a7163222c62d",
+    "url": "/static/js/runtime-main.ef0bb6e6.js"
+  },
+  {
+    "revision": "f7079923431667c88308ab6924ac8de4",
+    "url": "/static/media/axell.f7079923.png"
+  },
+  {
+    "revision": "dfca6105b30072e5f4d16959187476bf",
+    "url": "/static/media/portfolio-alura-html5-css3.dfca6105.png"
+  },
+  {
+    "revision": "75189c22d4deb3f5caac9adcc3a96b1b",
+    "url": "/static/media/portfolio-graphs.75189c22.png"
+  },
+  {
+    "revision": "1d87bc537f8f8d62f134d6403db83355",
+    "url": "/static/media/portfolio-omnistack10-devradar-frontend.1d87bc53.jpeg"
+  },
+  {
+    "revision": "bbcf4e492d35147c0825c068559b1bfa",
+    "url": "/static/media/portfolio-omnistack10-devradar-mobile.bbcf4e49.jpeg"
+  },
+  {
+    "revision": "73285578238460d0ae7719fd63ca9d0e",
+    "url": "/static/media/portfolio-pingcrc-maquinas.73285578.png"
+  },
+  {
+    "revision": "cc3ec10c9c65dc573652da54a64ea73b",
+    "url": "/static/media/portfolio-star-wars-lddm.cc3ec10c.png"
+  },
+  {
+    "revision": "88757ed26853cbacd1b80e2b179ceb94",
+    "url": "/static/media/portfolio-starwarsapp-levelup.88757ed2.png"
+  },
+  {
+    "revision": "25db489f775a5f95a89efef76629f348",
+    "url": "/static/media/portfolio-super-store.25db489f.png"
+  }
+]);
