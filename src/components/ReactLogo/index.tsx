28fc0f5d54c4ce677b49c2b005bc53e43bf7e054
axell-brendow <breno.axel@gmail.com>
2020-04-24 03:33:46 -0300

    Add ReactLogo

---
+++ src/components/ReactLogo/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { ReactLogoStyle } from "./styles";
+
+const ReactLogo = () => (
+  <ReactLogoStyle title={"Go to React Page"}>
+    <span className="nucleo" />
+  </ReactLogoStyle>
+);
+
+export default ReactLogo;
